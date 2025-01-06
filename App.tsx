ed0012f0f9434294ce1564b889d7efb04bf14189
Arush Singh <sarushmad@gmail.com>
2025-01-06 09:58:32 +0530

    updated configs to fix build errors

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { AppNavigator } from "./src/navigation/AppNavigator";
+import { View } from "react-native";
 
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!);
 
 export default function App() {
   return (
-    <ConvexProvider client={convex}>
-      <AppNavigator />
-    </ConvexProvider>
+    <View style={{ flex: 1 }}>
+      <ConvexProvider client={convex}>
+        <AppNavigator />
+      </ConvexProvider>
+    </View>
   );
 }
--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -10,60 +10,63 @@ import { AdminDashboard } from "../screens/AdminDashboard";
 import { NavigationContainer } from "@react-navigation/native";
 import { EditProfileScreen } from "../screens/EditProfileScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View } from "react-native";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const AppNavigator = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: "#FFFFFF",
-          },
-          headerTintColor: "#000000",
-        }}
-      >
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Register"
-          component={RegisterScreen}
-          options={{ title: "Register" }}
-        />
-        <Stack.Screen
-          name="AdminDashboard"
-          component={AdminDashboard}
-          options={({ navigation }) => ({
-            header: () => <Header navigation={navigation} isAdmin={true} />,
-          })}
-        />
-        <Stack.Screen
-          name="UserDashboard"
-          component={UserDashboard}
-          options={({ navigation }) => ({
-            header: () => <Header navigation={navigation} isAdmin={false} />,
-          })}
-        />
-        <Stack.Screen
-          name="AddLoad"
-          component={AddLoadScreen}
-          options={{ title: "Add New Load" }}
-        />
-        <Stack.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{ title: "Profile" }}
-        />
-        <Stack.Screen
-          name="EditProfile"
-          component={EditProfileScreen}
-          options={{ title: "Edit Profile" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#FFFFFF",
+            },
+            headerTintColor: "#000000",
+          }}
+        >
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Register"
+            component={RegisterScreen}
+            options={{ title: "Register" }}
+          />
+          <Stack.Screen
+            name="AdminDashboard"
+            component={AdminDashboard}
+            options={({ navigation }) => ({
+              header: () => <Header navigation={navigation} isAdmin={true} />,
+            })}
+          />
+          <Stack.Screen
+            name="UserDashboard"
+            component={UserDashboard}
+            options={({ navigation }) => ({
+              header: () => <Header navigation={navigation} isAdmin={false} />,
+            })}
+          />
+          <Stack.Screen
+            name="AddLoad"
+            component={AddLoadScreen}
+            options={{ title: "Add New Load" }}
+          />
+          <Stack.Screen
+            name="Profile"
+            component={ProfileScreen}
+            options={{ title: "Profile" }}
+          />
+          <Stack.Screen
+            name="EditProfile"
+            component={EditProfileScreen}
+            options={{ title: "Edit Profile" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
   );
 };
